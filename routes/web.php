cf0e2fa73ba2cec15e2568fbedd9d8f69e2cf370
Heya Na <naheya1126@gmail.com>
2017-08-01 22:37:34 -0400

    show tasks

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', function () {
+	$tasks = App\Task::all();
+
+    return view('tasks.index',compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+	$task = App\Task::find($id);
+
+    return view('tasks.show',compact('task'));
+});
